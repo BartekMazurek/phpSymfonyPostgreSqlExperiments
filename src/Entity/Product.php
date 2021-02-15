ce36d9d52c39acf5f98f0eab8d0278914db51b81
BartekMazurek <bartlomiejmazurek9@gmail.com>
2021-02-15 18:59:11 +0100

    Added product entity & repository

---
+++ src/Entity/Product.php
@@ -0,0 +1,137 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Entity;
+
+use App\Repository\ProductRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(
+ *     repositoryClass=ProductRepository::class
+ * )
+ * @ORM\Table(
+ *     name="`products`",
+ *     schema="shop"
+ * )
+ */
+class Product
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(
+     *     type="integer"
+     * )
+     */
+    private int $id;
+
+    /**
+     * @ORM\Column(
+     *     type="string",
+     *     length=50
+     * )
+     */
+    private string $name;
+
+    /**
+     * @ORM\Column(
+     *     type="text"
+     * )
+     */
+    private string $description;
+
+    /**
+     * @ORM\Column(
+     *     type="integer"
+     * )
+     */
+    private int $amount;
+
+    /**
+     * @ORM\Column(
+     *     type="float"
+     * )
+     */
+    private float $price;
+
+    /**
+     * @ORM\Column(
+     *     type="datetime"
+     * )
+     */
+    private \DateTimeInterface $createdAt;
+
+    /**
+     * @ORM\Column(
+     *     type="datetime"
+     * )
+     */
+    private \DateTimeInterface $modifiedAt;
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+
+    public function getDescription(): string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): void
+    {
+        $this->description = $description;
+    }
+
+    public function getAmount(): int
+    {
+        return $this->amount;
+    }
+
+    public function setAmount(int $amount): void
+    {
+        $this->amount = $amount;
+    }
+
+    public function getPrice(): float
+    {
+        return $this->price;
+    }
+
+    public function setPrice(float $price): void
+    {
+        $this->price = $price;
+    }
+
+    public function getCreatedAt(): \DateTimeInterface
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeInterface $createdAt): void
+    {
+        $this->createdAt = $createdAt;
+    }
+
+    public function getModifiedAt(): \DateTimeInterface
+    {
+        return $this->modifiedAt;
+    }
+
+    public function setModifiedAt(\DateTimeInterface $modifiedAt): void
+    {
+        $this->modifiedAt = $modifiedAt;
+    }
+}
